62df7cb2c1f71d95633c4f682367c04893a197cd
Victor <mhpd100.i@gmail.com>
2023-04-14 19:56:51 +0300

    Practice part 1. Start creating app. Done

--- js/script.js
+++ js/script.js
@@ -23,3 +23,27 @@
 
 'use strict';
 
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", ""); // #1
+
+const personalMovieDB = {                                             // #2
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const answer1 = prompt("Один из последних просмотренных фильмов?", ""),
+      answer2 = prompt("На сколько оцените его?", ""),
+      answer3 = prompt("Один из последних просмотренных фильмов?", ""),
+      answer4 = prompt("На сколько оцените его?", "");
+
+personalMovieDB.movies[answer1] = answer2;
+personalMovieDB.movies[answer3] = answer4;
+
+console.log(personalMovieDB);
+
+
+
+
+

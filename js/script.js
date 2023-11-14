4782576b6f9b822f98779c57391e3d26a0aac8c5
Victor <mhpd100.i@gmail.com>
2023-11-14 12:53:58 +0200

    Practice lesson ot. 5

--- js/script.js
+++ js/script.js
@@ -1,100 +1,89 @@
-/* Задание на урок:
+/* Задания на урок:
 
-1) У нас уже есть рабочее приложение, состоящее из отдельных функций. Представьте, что
-перед вами стоит задача переписать его так, чтобы все функции стали методами объекта personalMovieDB
-Такое случается в реальных продуктах при смене технологий или подхода к архитектуре программы
+1) Удалить все рекламные блоки со страницы (правая часть сайта)
 
-2) Создать метод toggleVisibleMyDB, который при вызове будет проверять свойство privat. Если оно false - он
-переключает его в true, если true - переключает в false. Протестировать вместе с showMyDB.
+2) Изменить жанр фильма, поменять "комедия" на "драма"
 
-3) В методе writeYourGenres запретить пользователю нажать кнопку "отмена" или оставлять пустую строку. 
-Если он это сделал - возвращать его к этому же вопросу. После того, как все жанры введены - 
-при помощи метода forEach вывести в консоль сообщения в таком виде:
-"Любимый жанр #(номер по порядку, начиная с 1) - это (название из массива)"*/
+3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
+Реализовать только при помощи JS
+
+4) Список фильмов на странице сформировать на основании данных из этого JS файла.
+Отсортировать их по алфавиту 
+
+5) Добавить нумерацию выведенных фильмов */
 
 'use strict';
 
-const personalMovieDB = {                                             
-    count: 0,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: true,
-
-    start: () => {
-        personalMovieDB.count = +prompt("Сколько фильмов вы уже посмотрели?", "");
-
-        while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
-            personalMovieDB.count = +prompt("Сколько фильмов вы уже посмотрели?", "");
-        }
-    },
-
-    rememberMyFilms: function() {
-        for (let i = 0; i < 1; i++) {
-            const answer1 = prompt("Один из последних просмотренных фильмов?", ""),
-                    answer2 = prompt("На сколько оцените его?", "");
-        
-            if (answer1 != null && answer2 != null && answer1 != '' && answer2 != '' && answer1.length < 50 && answer2.length < 50) {
-                personalMovieDB.movies[answer1] = answer2;
-                console.log('done');
-            } else {
-                console.log('error');
-                i--;
-            }
-        }
-    },
-
-    detectPersonalLevel: function() {
-        if (personalMovieDB.count < 10) {
-            alert('Просмотрено довольно мало фильмов');
-        } else if (personalMovieDB.count >=10 && personalMovieDB.count <= 30) {
-            alert('Вы классический зритель');
-        } else if (personalMovieDB.count > 30) {
-            alert('Вы киноман');
-        } else {
-            alert('Произошла ошибка');
-        }
-    },
-
-    writeYourGenres: function() {
-        for (let index = 1; index < 2; index++) {
-            // let genre = prompt(`Ваш любимый жанр под номером ${index}`, "");
-
-            // if(genre == '' || genre == null) {
-            //     alert(`Вы ввели некорректные данные или не ввели их вовсе`);
-            //     index--;
-            // } else {
-            //     personalMovieDB.genres[index - 1] = genre;
-            // }
-
-            let genres = prompt(`Введите ваши любимые жанры через запятую`, "");
-             if(genres == '' || genres == null) {
-                alert(`Вы ввели некорректные данные или не ввели их вовсе`);
-                index--;
-            } else {
-                personalMovieDB.genres = genres.split(', ');
-                personalMovieDB.genres.sort();
-            }
-        }
-
-        personalMovieDB.genres.forEach((item, i) => {
-            console.log(`Любимый жанр #${i + 1} - это ${item}`);
-        });
-    },
-
-    showMyDB: function(hidden) {
-        if (!hidden)
-            console.log(personalMovieDB);
-    },
-
-    toggleVisibleMyDB: function() {
-        this.privat = !this.privat;
-    }
+const movieDB = {
+    movies: [
+        "Логан",
+        "Лига справедливости",
+        "Ла-ла лэнд",
+        "Одержимость",
+        "Скотт Пилигрим против..."
+    ]
 };
 
-// personalMovieDB.start();
-// personalMovieDB.rememberMyFilms();
-// personalMovieDB.detectPersonalLevel();
-// personalMovieDB.writeYourGenres();
-// personalMovieDB.toggleVisibleMyDB();
-// personalMovieDB.showMyDB(personalMovieDB.privat);
+
+// my code:
+// // 1st task
+// const divPromo = document.querySelector('.promo__adv');
+// const imgPromo = divPromo.querySelectorAll('img');
+
+// imgPromo.forEach(item => {
+//     item.remove();
+// });
+
+// // 2nd task
+// const divChangeGenre = document.querySelector('.promo__genre');
+// divChangeGenre.innerText = "Драма";
+
+// // 3rd task
+// const divChangeBG = document.querySelector('.promo__bg');
+// divChangeBG.style.background = 'url(img/bg.jpg) center center/cover no-repeat';
+
+// // 4th task
+// const listOfRecentMovies = document.querySelectorAll('.promo__interactive-item');
+// let arrayOfMovies = [], NodeIterator = 0;
+
+// for (let i = 0; i < listOfRecentMovies.length; i++) {
+//     arrayOfMovies[i] = listOfRecentMovies[i].innerText;
+// }
+
+// arrayOfMovies.sort();
+
+// for (let i = 0; i < listOfRecentMovies.length; i++) {
+//     listOfRecentMovies[i].innerText = arrayOfMovies[i];
+// }
+
+// // 5th task
+// for (let i = 0; i < listOfRecentMovies.length; i++) {
+//     listOfRecentMovies[i].innerText = `${i + 1}. ` + listOfRecentMovies[i].innerText;
+// }
+
+// code from lesson:
+
+const adv = document.querySelectorAll('.promo__adv img'),
+      poster = document.querySelector('.promo__bg'),
+      genre = poster.querySelector('.promo__genre'),
+      movieList = document.querySelector('.promo__interactive-list');
+
+adv.forEach(advItem => {
+    advItem.remove();
+});
+
+genre.textContent = 'драма';
+
+poster.style.backgroundImage = 'url("img/bg.jpg")';
+
+movieList.innerHTML = "";
+
+movieDB.movies.sort();
+
+movieDB.movies.forEach((film, i) => {
+    movieList.innerHTML += `
+        <li class="promo__interactive-item"> ${i + 1} ${film}
+            <div class="delete"></div>
+        </li>   
+    `;
+});
